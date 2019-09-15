88f5ee1a7eeaa282f875104cbbddae14e9a56207
angelicty <muyenziraissa@gmail.com>
2019-09-15 18:07:37 +0200

    Creating index function

--- app/views.py
+++ app/views.py
@@ -1,14 +1,15 @@
 from flask import render_template
 from app import app
 
-# Views
-# @app.route('/')
-# def index():
+Views
+@app.route('/')
+def index():
 
-#     '''
-#     View root page function that returns the index page and its data
-#     '''
-#     return render_template('index.html')
+    '''
+    View root page function that returns the index page and its data
+    '''
+    title = ' Welcome to online news highlight website'
+    return render_template('index.html', title = title)
 @app.route('/source/<int:id>')
 def source(id):
 
