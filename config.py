e715711fa7b0cfa57e1a7b638a295cb3482754eb
angelicty <muyenziraissa@gmail.com>
2019-09-15 16:47:27 +0200

    Adding configuration

---
+++ config.py
@@ -0,0 +1,20 @@
+import os
+
+class Config:
+
+    SOURCE_API_BASE_URL ='https://newsapi.org/v2/sources{}?apiKey={}'
+    SOURCE_API_KEY = os.environ.get('SOURCE_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
