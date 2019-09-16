2ad8b57ac8bd9ea4ddcdc263d529211020a64d4d
angelicty <muyenziraissa@gmail.com>
2019-09-16 21:24:18 +0200

    Displaying news source

--- app/requests.py
+++ app/requests.py
@@ -22,15 +22,15 @@ def get_source(category):
 	Function that gets the json response to our url request
 	'''
     get_source_url = base_url.format(category,api_key)
-
+    print(get_source_url)
     with urllib.request.urlopen(get_source_url) as url:
         get_source_data = url.read()
         get_source_response = json.loads(get_source_data)
 
         source_results = None
 
-        if get_source_response['results']:
-            source_results_list = get_source_response['results']
+        if get_source_response['sources']:
+            source_results_list = get_source_response['sources']
             source_results = process_results(source_results_list)
 
 
